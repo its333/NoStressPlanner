55828dd97e89dd27f15706422dcd1c2894211a79
its333 <useforegister@gmail.com>
2025-09-23 23:47:48 -0400

    Fix Content Security Policy for inline scripts

--- next.config.js
+++ next.config.js
@@ -8,6 +8,20 @@ const nextConfig = {
     // Disable TypeScript errors during builds
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value:
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://js.pusher.com; object-src 'none'; base-uri 'self';",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
